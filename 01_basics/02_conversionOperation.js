002cd42a16705a61fcb494096e527067f2ac9a09
YashLearner <157092219+YashLearner@users.noreply.github.com>
2024-01-19 06:38:11 +0000

    Updates code

--- 01_basics/02_conversionOperation.js
+++ 01_basics/02_conversionOperation.js
@@ -14,8 +14,32 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
 
-let someNumber = undefined;
+// let someNumber = undefined ;
 
-let convert = String(someNumber);
-console.log(convert);
-console.log(typeof convert);
+// let convert = String(someNumber);
+// console.log(convert);
+// console.log(typeof convert);
+
+
+
+// ********************** Opearations ******************** //
+
+let val = 3;
+let negVal= -val;
+console.log(negVal);  
+
+let str1 = "Yashraj"
+let str2 = "Songara"
+
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log(1 + 34+  "2" + 2);
+
+// console.log(1 + 1 + "2");
+
+
+// console.log("1" + 2 + 2);
+
