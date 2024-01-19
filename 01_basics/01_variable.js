208ad3e9c3b8111eefb28ce744c7dbbb242d2b0d
YashLearner <157092219+YashLearner@users.noreply.github.com>
2024-01-19 06:23:35 +0000

    Conversion of datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 973429;
+let accountEmail = "yashuu4522@gmail.com";
+var accountPassword = "23154";
+accountCity = "Jaipur";
+
+accountEmail = "lhsdujf,sn"
+accountPassword = "1541384"
+accountCity = "Benguluru"
+// accountId = 53135;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity]);
---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,21 @@
+
+// let score = true;
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let isLoggedIn = 0;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = undefined;
+
+let convert = String(someNumber);
+console.log(convert);
+console.log(typeof convert);
